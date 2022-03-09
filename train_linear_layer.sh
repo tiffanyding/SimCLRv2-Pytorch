24b2f8feb6036fef1294c9641e6a192f2a823d49
Tiffany Ding <tiffany_ding@berkeley.edu>
2022-03-09 10:48:47 -0800

    [commit representations]

---
+++ train_linear_layer.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Run this file using "sbatch my_script.sh"
+
+# the SBATCH directives must appear before any executable
+# line in this script
+
+#SBATCH -p rise # partition (queue)
+#SBATCH --cpus-per-task=72 # number of cores per task
+# I think gpu:8 will request 8 of any kind of gpu per node,
+# and gpu:v100_32:8 should request 8 v100_32 per node
+#SBATCH --mem-per-cpu=4G
+#SBATCH --gres=gpu:1
+#SBATCH --exclude=freddie,flaminio,blaze # nodes not yet on SLURM-only
+#SBATCH -t 0-24:00 # time requested (D-HH:MM)
+# slurm will cd to this directory before running the script
+# you can also just run sbatch submit.sh from the directory
+# you want to be in
+#SBATCH -D /home/eecs/tiffany_ding/code/SimCLRv2-Pytorch
+# use these two lines to control the output file. Default is
+# slurm-<jobid>.out. By default stdout and stderr go to the same
+# place, but if you use both commands below they'll be split up
+# filename patterns here: https://slurm.schedmd.com/sbatch.html
+# %N is the hostname (if used, will create output(s) per node)
+# %j is jobid
+#SBATCH -o /home/eecs/tiffany_ding/slurm_output/slurm.%N.%j.out # STDOUT
+#SBATCH -e /home/eecs/tiffany_ding/slurm_output/slurm.%N.%j.err # STDERR
+# if you want to get emails as your jobs run/fail
+##SBATCH --mail-type=NONE # Mail events (NONE, BEGIN, END, FAIL, ALL)
+##SBATCH --mail-user=tiffany_ding@eecs.berkeley.edu # Where to send mail
+#seff $SLURM_JOBID
+# print some info for context
+pwd | xargs -I{} echo "Current directory:" {}
+hostname | xargs -I{} echo "Node:" {}
+python train_linear_layer.py r152_3x_sk1.pth --num-workers 4 --num-epochs 5 --lr 0.5
